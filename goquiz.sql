27e834d723e58da81f84c278075ac62f3148050a
fauzan-radji <triputrafauzan@gmail.com>
2022-10-06 20:20:03 +0800

    exporting database

--- goquiz.sql
+++ goquiz.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 04 Okt 2022 pada 15.17
--- Versi server: 10.4.22-MariaDB
--- Versi PHP: 8.1.2
+-- Generation Time: Oct 06, 2022 at 02:17 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,43 +24,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `history`
+-- Table structure for table `history`
 --
 
 CREATE TABLE `history` (
   `id` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
-  `point` int(11) NOT NULL,
-  `finish_time` varchar(30) NOT NULL
+  `point` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `history`
+-- Dumping data for table `history`
 --
 
-INSERT INTO `history` (`id`, `id_user`, `point`, `finish_time`) VALUES
-(1, 2, 8, ''),
-(2, 2, 5, ''),
-(3, 2, 10, ''),
-(4, 2, 5, ''),
-(5, 2, 3, ''),
-(6, 2, 10, '1664855484922'),
-(7, 2, 10, '1664855585430'),
-(8, 2, 10, '1664855769787'),
-(9, 2, 0, '1664883644414'),
-(10, 2, 10, '1664885312000'),
-(11, 2, 20, '1664885560501'),
-(12, 2, 20, '1664887424434'),
-(13, 2, 10, '1664887473434'),
-(14, 2, 30, '1664887504796'),
-(15, 2, 0, '1664887574552'),
-(16, 2, 20, '1664888325221'),
-(17, 2, 20, '1664889278953');
+INSERT INTO `history` (`id`, `id_user`, `point`) VALUES
+(1, 2, 8),
+(2, 2, 5),
+(3, 2, 10),
+(4, 2, 5),
+(5, 2, 3);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `profile`
+-- Table structure for table `profile`
 --
 
 CREATE TABLE `profile` (
@@ -68,87 +55,88 @@ CREATE TABLE `profile` (
   `nama` varchar(40) NOT NULL,
   `jenis_kelamin` varchar(10) NOT NULL,
   `role` varchar(10) NOT NULL,
-  `exp` int(11) NOT NULL,
+  `kyu` int(50) NOT NULL,
   `username` varchar(20) NOT NULL,
   `password` char(10) NOT NULL,
   `profile` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `profile`
+-- Dumping data for table `profile`
 --
 
-INSERT INTO `profile` (`id_profile`, `nama`, `jenis_kelamin`, `role`, `exp`, `username`, `password`, `profile`) VALUES
-(2, 'fauzan', '', '', 150, 'fauzan', '12345', '');
+INSERT INTO `profile` (`id_profile`, `nama`, `jenis_kelamin`, `role`, `kyu`, `username`, `password`, `profile`) VALUES
+(2, 'fauzan', '', '', 0, 'fauzan', '12345', ''),
+(3, 'fauzan', '', '', 0, 'fauzan', '456', '');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `question`
+-- Table structure for table `question`
 --
 
 CREATE TABLE `question` (
   `id_question` int(11) NOT NULL,
   `question` varchar(300) NOT NULL,
-  `answer_a` varchar(100) NOT NULL,
-  `answer_b` varchar(100) NOT NULL,
-  `answer_c` varchar(100) NOT NULL,
-  `answer_d` varchar(100) NOT NULL,
+  `answer_a` varchar(50) NOT NULL,
+  `answer_b` varchar(50) NOT NULL,
+  `answer_c` varchar(50) NOT NULL,
+  `answer_d` varchar(50) NOT NULL,
   `is_correct` char(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `question`
+-- Dumping data for table `question`
 --
 
 INSERT INTO `question` (`id_question`, `question`, `answer_a`, `answer_b`, `answer_c`, `answer_d`, `is_correct`) VALUES
 (1, 'Yang manakah merupakan bahasa daerah gorontalo dari \"Makan\"?', 'O\'ato', 'Malita', 'Monga', 'Landingalo', 'c'),
-(3, 'Kata \"Membajak\" jika di terjemahkan kedalam bahasa daerah Gorontalo berarti ...', 'Momade\'o', 'Ta\'apo', 'Mohalingo', 'Mowali', 'a'),
-(4, 'Kata \"Ponula\" jika di terjemahkan kedalam bahasa Indonesia', 'Ayam', 'Manusia', 'Raja', 'Ikan', 'd'),
+(3, 'Arti \"Membajak\" jika di terjemahkan kedalam bahasa daerah Gorontalo adalah ...', 'Momade\'o', 'Ta\'apo', 'Mohalingo', 'Mowali', 'a'),
+(4, '\"Ponula\" jika di terjemahkan kedalam bahasa Indonesia adalah ...', 'Ayam', 'Manusia', 'Raja', 'Ikan', 'd'),
 (5, 'Wolo tugasi li pak Polisi?', 'Belajar', 'Makan', 'Menilang', 'Pungli', 'c'),
-(6, 'Kata \"Patodu\" jika di terjemahkan kedalam bahasa Indonesia adalah ...', 'Tebu', 'Daun pandan', 'Motor', 'Sepeda', 'a'),
-(7, '\"Tembe\" jika di terjemahkan kedalam bahasa gorontalo berarti ...', 'Pinang', 'Sirih', 'Ikan ', 'Tembaga', 'b');
+(6, '\"Patodu\" jika di terjemahkan kedalam bahasa indonesia adalah ...', 'Tebu', 'Daun pandan', 'Motor', 'Sepeda', 'a'),
+(7, 'Tembe jika di terjemahkan kedalam bahasa gorontalo merupakan ...', 'Pinang', 'Sirih', 'Ikan ', 'Tembaga', 'b');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `history`
+-- Indexes for table `history`
 --
 ALTER TABLE `history`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `profile`
+-- Indexes for table `profile`
 --
 ALTER TABLE `profile`
   ADD PRIMARY KEY (`id_profile`);
 
 --
--- Indeks untuk tabel `question`
+-- Indexes for table `question`
 --
 ALTER TABLE `question`
   ADD PRIMARY KEY (`id_question`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `history`
+-- AUTO_INCREMENT for table `history`
 --
 ALTER TABLE `history`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT untuk tabel `profile`
+-- AUTO_INCREMENT for table `profile`
 --
 ALTER TABLE `profile`
   MODIFY `id_profile` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT untuk tabel `question`
+-- AUTO_INCREMENT for table `question`
 --
 ALTER TABLE `question`
   MODIFY `id_question` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
